ffa2d3a26c27df90eb31aacc9e2b495096ec55bb
Richie <molunorichie@gmail.com>
2022-09-06 21:25:34 +0100

    add: routes for login, signup and password-reset

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express')
-const router = express.Router
+const router = express.Router()
 
+const { signup, verifyEmail, login, passwordReset, confirmResetAndChangePassword } = require('../controllers/authController')
 
 router.
-    post('/signup').
-    post('/login').
-    post('/passwordreset').
-    post('/confirmreset')
+    post('/signup', signup).
+    post('/verify', verifyEmail).
+    post('/login', login)
+
+router.
+    post('/password/reset', passwordReset).
+    put('/password/confirmtoken', confirmResetAndChangePassword)
+
+// Post login routes should require user.isActive
 
 module.exports = router
